56abf1ce520e2068da71eccd82e35702e8ef3a60
isling98 <markushaag@live.dk>
2019-10-28 11:32:35 +0100

    Lagt billeder ind i res og sat op til comic_activity.

--- app/src/main/java/com/example/nepalappgroupb2/MainActivity.java
+++ app/src/main/java/com/example/nepalappgroupb2/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.nepalappgroupb2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button comic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        comic = findViewById(R.id.comicButton);
+        comic.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View v){
+        if (v == comic){
+            Intent intent = new Intent(this, ComicActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
