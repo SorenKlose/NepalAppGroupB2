b643e7850a50b04feb7e3bf1fbff42bee837c2ba
Asger <Asgerakerman@gmail.com>
2019-10-28 11:43:58 +0100

    Har forsøgt at lave et popup vindue
    Har forsøgt at lave et popup vindue, med sharedpreferences. Med det har jeg prøvet at få popup vinduet til kun at komme op en gang, når man først åbner appen. Den spørger derefter om hvilket profil man skal oprette

--- app/src/main/java/com/example/nepalappgroupb2/ProfileActivity.java
+++ app/src/main/java/com/example/nepalappgroupb2/ProfileActivity.java
@@ -2,19 +2,30 @@ package com.example.nepalappgroupb2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    SharedPreferences mPrefs;
+    final String welcomeScreenShownPref = "welcomeScreenShown";
+
     private static final String TAG = "MainActivity";
 
     private TextView mDisplayDate;
@@ -26,6 +37,47 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+  //FORSØG PÅ POPUP DER KUN KOMMER EN GANG
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // second argument is the default to use if the preference can't be found
+        Boolean welcomeScreenShown = mPrefs.getBoolean(welcomeScreenShownPref, false);
+
+        if (!welcomeScreenShown) {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(ProfileActivity.this);
+            View mView = getLayoutInflater().inflate(R.layout.popup, null);
+            final ImageView mom = (ImageView) mView.findViewById(R.id.iwMom);
+            final ImageView pregnate = (ImageView) mView.findViewById(R.id.iwPregnate);
+            mBuilder.setView(mView);
+            mBuilder.setView(mView);
+            final AlertDialog dialog = mBuilder.create();
+            dialog.show();
+
+            mom.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                  //  Intent myIntent = new Intent(ProfileActivity.this, Profile2Acitivty.class);
+                  //  ProfileActivity.this.startActivity(myIntent);
+                    dialog.dismiss();
+                }});
+            pregnate.setOnClickListener(new View.OnClickListener(){
+                public void onClick (View view){
+                    dialog.dismiss();
+                }
+            });
+        }
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.putBoolean(welcomeScreenShownPref, true);
+        editor.commit();
+
+
+
+
+
+    //DATE PICKER
         mDisplayDate = (TextView) findViewById(R.id.tvDate);
 
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
