8f004824e462cfd79137a60c1e3c2121028333fc
Andreas Hyldahl <Andreas.Hyldahl10@hotmail.com>
2019-10-31 11:31:25 +0100

    Layout for homepagen med intens til de forskellige sider.

--- app/src/main/java/com/example/nepalappgroupb2/MainActivity.java
+++ app/src/main/java/com/example/nepalappgroupb2/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.nepalappgroupb2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView calenderButton;
+    ImageView recipesButton;
+    ImageView comicsButton;
+    ImageView quizButton;
+
+    ImageView profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calenderButton = (ImageView) findViewById(R.id.calender);
+        recipesButton = (ImageView) findViewById(R.id.recipes);
+        comicsButton = (ImageView) findViewById(R.id.comics);
+        quizButton = (ImageView) findViewById(R.id.quiz);
+        profileButton = (ImageView) findViewById(R.id.profile);
+
+        calenderButton.setOnClickListener(this);
+        recipesButton.setOnClickListener(this);
+        comicsButton.setOnClickListener(this);
+        quizButton.setOnClickListener(this);
+        profileButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == calenderButton) {
+            Intent i = new Intent(this, Calender.class);
+            startActivity(i);
+        }
+        if (view == recipesButton) {
+            Intent i = new Intent(this, Recipes.class);
+            startActivity(i);
+        }
+        if (view == comicsButton) {
+            Intent i = new Intent(this, Comics.class);
+            startActivity(i);
+        }
+        if (view == quizButton) {
+            Intent i = new Intent(this, Quiz.class);
+            startActivity(i);
+        }
     }
 }
---
+++ app/src/main/java/com/example/nepalappgroupb2/ProgressBarFragment.java
@@ -0,0 +1,55 @@
+package com.example.nepalappgroupb2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Calendar.getInstance;
+
+public class ProgressBarFragment extends Fragment {
+
+    private ProgressBar progressBar;
+
+
+
+
+
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_progress_bar, container, false);
+
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+
+        long currentDate = Calendar.getInstance().getTimeInMillis();
+        // Calendar birthDate = new GregorianCalendar();
+
+        long birthDate = 25;
+
+        long progressInDays = TimeUnit.MILLISECONDS.toDays(currentDate - birthDate);
+
+
+        progressBar.setProgress((int) progressInDays); // progressStatus
+
+        return view;
+
+
+    }
+
+}
