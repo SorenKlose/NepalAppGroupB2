70680ee9da87f394733001137c66b9fb5b815e9d
NBK <msungo85@gmail.com>
2019-11-25 13:59:50 +0100

    tilføjet instrukmenttest på filterlist [virker ikke helt endnu]

---
+++ app/src/androidTest/java/com/example/nepalappgroupb2/FilterlistInstrumentedTest.kt
@@ -0,0 +1,30 @@
+package com.example.nepalappgroupb2
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.clearText
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FilterlistInstrumentedTest{
+@get:Rule
+var activityRule: ActivityTestRule<RecipeActivity> = ActivityTestRule(RecipeActivity::class.java)
+
+    @Test
+    fun input_to_Livedata(){
+        onView(withId(R.id.searchWordFragment)).check(matches(isDisplayed()))
+        onView(withId(R.id.searchWordFragment)).perform(clearText(),typeText( "abcdDCBA8"))
+
+        onView(withId(R.id.searchWordFragment)).check(matches(withText(activityRule.activity.getSearchWord().getValue())) )
+
+        activityRule.activity.finish()
+
+    }
+
+}
