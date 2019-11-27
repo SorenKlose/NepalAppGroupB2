abedf3b88f1036253a7575829411e891e7a4650d
Asger <Asgerakerman@gmail.com>
2019-11-27 17:11:03 +0100

    Numberpicker til pregnate profilen
    Numberpicker til pregnate profilen

--- app/src/main/java/com/example/nepalappgroupb2/Profile2Activity.java
+++ app/src/main/java/com/example/nepalappgroupb2/Profile2Activity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class Profile2Activity extends AppCompatActivity {
 
@@ -18,6 +20,12 @@ public class Profile2Activity extends AppCompatActivity {
 
 
         ImageView imFaneP = (ImageView) findViewById(R.id.imFaneP);
+        NumberPicker np = findViewById(R.id.numberPicker);
+        np.setMinValue(1);
+        np.setMaxValue(9);
+
+        np.setOnValueChangedListener(onValueChangeListener);
+
         imFaneP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,7 +33,19 @@ public class Profile2Activity extends AppCompatActivity {
                 Profile2Activity.this.startActivity(myIntent);
                 finish();
             }});
+
     }
+
+    NumberPicker.OnValueChangeListener onValueChangeListener = new NumberPicker.OnValueChangeListener(){
+        @Override
+        public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+            Toast.makeText(Profile2Activity.this,
+                    "selected number "+numberPicker.getValue(), Toast.LENGTH_SHORT);
+            int timePreg =  i1;
+            System.out.println(i1);
+
+        }
+    };
 
 
 }
