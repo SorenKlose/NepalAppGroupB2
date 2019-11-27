45ee2ff712340a4b967094b854a18f622daf86dc
Patrick <patrick.lopdrup@gmail.com>
2019-11-27 21:42:22 +0100

    lavet flere metoder til at få data ud fra Google Sheet og kommenteret alle sammen med JavaDoc comments

--- app/src/main/java/DataFromSheets.java
+++ app/src/main/java/DataFromSheets.java
@@ -9,18 +9,31 @@ import java.util.Map;
 public class DataFromSheets {
     private Map<String, String> map;
 
+    /**
+     * We are using this Google Sheet: https://docs.google.com/spreadsheets/d/1XV2U8gztCXR4BmPuLT1eQBxYUx0GFPDJp9S8YqRn92A/edit#gid=0
+     */
+
     public static void main(String[] args) {
         DataFromSheets data = new DataFromSheets();
         try {
             data.fromSheets();
             data.getMsgEngWithWeek(24);
             System.out.println(data.getMsgNum(34));
+
+            System.out.println(data.getRadioTxtWithWeek(16));
+            System.out.println(data.getRadioUrlWithNum(1));
+            System.out.println(data.getRadioUrlWithWeek(12));
+            System.out.println(data.getGoalWithNum(36));
+            System.out.println(data.getGoalWithWeek(12));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static String dataFromURL(String url) throws IOException {
+    /**
+     * Taken from "Galgelogik" by Jakob Nordfalk
+     */
+    private static String dataFromURL(String url) throws IOException {
         System.out.println("Henter data fra " + url);
         BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
         StringBuilder sb = new StringBuilder();
@@ -32,20 +45,32 @@ public class DataFromSheets {
         return sb.toString();
     }
 
-    public int getAmountOfHeaders(String line) {
+    /**
+     * Method used to find amount of headers in the Google Sheet.
+     * Fx "MsgNum", "Week", "MsgEng" are headers.
+     * @param line a single row from the Google Sheet
+     * @return the amount of headers in the Google Sheet as an int
+     */
+    private int getAmountOfHeaders(String line) {
         int counter = 0;
         for (int i = 0; i < line.length(); i++) {
             if (line.charAt(i) == '\t') counter++;
+            //if 2 tabs occur side by side we return
             if (line.charAt(i) == '\t' && line.charAt(i + 1) == '\t') return counter;
         }
         return counter;
     }
 
+    /**
+     * The method that save the data from Google Sheet into a HashMap
+     * @throws Exception
+     */
     public void fromSheets() throws Exception {
         String id = "1XV2U8gztCXR4BmPuLT1eQBxYUx0GFPDJp9S8YqRn92A";
 
         System.out.println("Henter ord");
 
+        //export as tsv file (tab separated values)
         String data = dataFromURL("https://docs.google.com/spreadsheets/d/" + id + "/export?format=tsv&id=" + id);
         System.out.println("hej:\n" + data);
         int lineNum = 0;
@@ -57,14 +82,17 @@ public class DataFromSheets {
         for (String line : dataList) {
             String[] lineElements = line.split("\t");
             System.out.println("test: " + Arrays.toString(lineElements));
+            //saving the first row/line from Google Sheet as headerNames in a String[]
             if (lineNum++ == 0) {
                 int indexLimit = getAmountOfHeaders(line);
-                System.out.println("index er: " + indexLimit);
-                headerNames = line.split("\t", indexLimit); //first line in sheets become headernames
+                headerNames = line.split("\t", indexLimit);
                 String lastWord = headerNames[headerNames.length - 1];
-                headerNames[headerNames.length - 1] = lastWord.substring(0, lastWord.indexOf('\t')); //remove end of last word
+                //remove end of last word
+                headerNames[headerNames.length - 1] = lastWord.substring(0, lastWord.indexOf('\t'));
             }
 
+            //provide each cell in a row a headerName + lineIndex as key
+            //Fx "MsgNum0", "MsgNum5", MsgEng29".
             int rowNum = 0;
             for (String s : headerNames) {
                 try {
@@ -77,11 +105,14 @@ public class DataFromSheets {
             }
         }
 
-        System.out.println("hej: " + map.get("MsgEng0"));
-        System.out.println();
         System.out.println(map);
     }
 
+    /**
+     * To get the MsgNum when you only know the week
+     * @param week the week number from Google Sheet
+     * @return the MsgNum corresponding to the "week" parameter or -1 if not found
+     */
     public int getMsgNum(int week) {
         String searchKey = "Week";
         String key;
@@ -97,12 +128,22 @@ public class DataFromSheets {
         return -1;
     }
 
+    /**
+     * The best method to get the english message. Running as fast as the Hashmap.get function.
+     * @param msgNum the MsgNum from the Google Sheet
+     * @return The english message as a String for the given msgNum
+     */
     public String getMsgEngWithNum(int msgNum) {
         String key = "MsgEng" + msgNum;
-        System.out.println(map.get(key));
         return map.get(key);
     }
 
+    /**
+     * If you know the msgNum use the function "getMsgEngWithNum()" instead.
+     * This is O(n) to find the msgNum for the specific week.
+     * @param week the week number from Google Sheet
+     * @return the english message as a String or null if not found
+     */
     public String getMsgEngWithWeek(int week) {
         int msgNum = getMsgNum(week);
         try {
@@ -112,5 +153,114 @@ public class DataFromSheets {
         return null;
     }
 
+    /**
+     * The best method to get the nepali message. Running as fast as the Hashmap.get function.
+     * @param msgNum the MsgNum from the Google Sheet
+     * @return The nepali message as a String for the given msgNum
+     */
+    public String getMsgNepWithNum(int msgNum) {
+        String key = "MsgNep" + msgNum;
+        return map.get(key);
+    }
+
+    /**
+     * If you know the msgNum use the function "getMsgNepWithNum()" instead.
+     * This is O(n) to find the msgNum for the specific week.
+     * @param week the week number from Google Sheet
+     * @return the nepali message as a String or null if not found
+     */
+    public String getMsgNepWithWeek(int week) {
+        int msgNum = getMsgNum(week);
+        try {
+            return getMsgNepWithNum(msgNum);
+        } catch (NullPointerException e) {}
+
+        return null;
+    }
+
+    /**
+     * The best method to get the RadioTxt. Running as fast as the Hashmap.get function.
+     * @param msgNum the MsgNum from the Google Sheet
+     * @return The RadioTxt as a String for the given msgNum
+     */
+    public String getRadioTxtWithNum(int msgNum) {
+        String key = "RadioTxt" + msgNum;
+        String radioTxt = map.get(key);
+        return radioTxt.equals("") ? null : radioTxt;
+    }
+
+    /**
+     * If you know the msgNum use the function "getRadioTxtWithNum()" instead.
+     * This is O(n) to find the msgNum for the specific week.
+     * @param week the week number from Google Sheet
+     * @return the RadioTxt as a String or null if not found
+     */
+    public String getRadioTxtWithWeek(int week) {
+        int msgNum = getMsgNum(week);
+        try {
+            return getRadioTxtWithNum(msgNum);
+        } catch (NullPointerException e) {}
+
+        return null;
+    }
+
+    /**
+     * The best method to get the RadioUrl. Running as fast as the Hashmap.get function.
+     * @param msgNum the MsgNum from the Google Sheet
+     * @return The RadioUrl as a String for the given msgNum
+     */
+    public String getRadioUrlWithNum(int msgNum) {
+        String key = "RadioUrl" + msgNum;
+        String radioTxt = map.get(key);
+        return radioTxt.equals("") ? null : radioTxt;
+    }
+
+    /**
+     * If you know the msgNum use the function "getRadioUrlWithNum()" instead.
+     * This is O(n) to find the msgNum for the specific week.
+     * @param week the week number from Google Sheet
+     * @return the RadioUrl as a String or null if not found
+     */
+    public String getRadioUrlWithWeek(int week) {
+        int msgNum = getMsgNum(week);
+        try {
+            return getRadioUrlWithNum(msgNum);
+        } catch (NullPointerException e) {}
+
+        return null;
+    }
+
+    /**
+     * The best method to get the Goal message. Running as fast as the Hashmap.get function.
+     * @param msgNum the MsgNum from the Google Sheet
+     * @return The Goal message as a String for the given msgNum
+     */
+    public String getGoalWithNum(int msgNum) {
+        String key = "Goal" + msgNum;
+        return map.get(key);
+    }
+
+    /**
+     * If you know the msgNum use the function "getGoalWithNum()" instead.
+     * This is O(n) to find the msgNum for the specific week.
+     * @param week the week number from Google Sheet
+     * @return the Goal of the message as a String or null if not found
+     */
+    public String getGoalWithWeek(int week) {
+        int msgNum = getMsgNum(week);
+        try {
+            return getGoalWithNum(msgNum);
+        } catch (NullPointerException e) {}
+
+        return null;
+    }
+
+    /**
+     * To get the size of the HashMap
+     * @return size of HashMap
+     */
+    public int getMapSize() {
+        return map.size();
+    }
 
 }
