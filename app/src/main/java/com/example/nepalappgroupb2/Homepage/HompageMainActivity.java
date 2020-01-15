5dacc873741408532e609f61f734363a69b1f7dc
Patrick <patrick.lopdrup@gmail.com>
2020-01-15 17:48:26 +0100

    igang med widget

--- app/src/main/java/com/example/nepalappgroupb2/Homepage/HompageMainActivity.java
+++ app/src/main/java/com/example/nepalappgroupb2/Homepage/HompageMainActivity.java
@@ -2,8 +2,12 @@ package com.example.nepalappgroupb2.Homepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +20,8 @@ import com.example.nepalappgroupb2.Quiz.QuizActivity;
 import com.example.nepalappgroupb2.R;
 import com.example.nepalappgroupb2.Recipe.RecipeActivity;
 
+import java.util.Date;
+
 public class HompageMainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button calenderButton;
@@ -27,6 +33,15 @@ public class HompageMainActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences sp = getSharedPreferences("profile", Context.MODE_PRIVATE);
+        int month = sp.getInt("month", -1);
+        System.out.println("her er måned: " + month);
+        Date hej = new Date(System.currentTimeMillis());
+        System.out.println("måneder siden 1970: ");
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
