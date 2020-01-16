29a950ef177b1f8b175fe1b945370b430c8dcb3f
Asger <Asgerakerman@gmail.com>
2020-01-16 11:45:16 +0100

    Lavet notificationer, på en enkelt channel.
    Lige nu gør den det kun ved startup, next up er at finde ud af hvordan den kan gøre det løbende

--- app/src/main/java/com/example/nepalappgroupb2/Homepage/HompageMainActivity.java
+++ app/src/main/java/com/example/nepalappgroupb2/Homepage/HompageMainActivity.java
@@ -1,11 +1,17 @@
 package com.example.nepalappgroupb2.Homepage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
@@ -23,13 +29,14 @@ import com.example.nepalappgroupb2.Recipe.RecipeActivity;
 import java.util.Date;
 
 public class HompageMainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    public static final String CHANNEL_ID = "channel";
     Button calenderButton;
     Button recipesButton;
     Button comicsButton;
     Button quizButton;
     private ProgressBarFragment progressBar;
     Button profileButton;
+    private NotificationManagerCompat notiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +52,7 @@ public class HompageMainActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        notiManager = NotificationManagerCompat.from(this);
         calenderButton = (Button) findViewById(R.id.btnCalendar);
         recipesButton = (Button) findViewById(R.id.btnRecipe);
         comicsButton = (Button) findViewById(R.id.btnComics);
@@ -56,6 +64,8 @@ public class HompageMainActivity extends AppCompatActivity implements View.OnCli
         comicsButton.setOnClickListener(this);
         quizButton.setOnClickListener(this);
         profileButton.setOnClickListener(this);
+
+        sendNoti();
 
 
     }
@@ -91,4 +101,28 @@ public class HompageMainActivity extends AppCompatActivity implements View.OnCli
         progressBar.update();
 
     }
+    public void sendNoti() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Channel",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel.setDescription("Channel");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.push_icon)
+                .setContentTitle("NEW")
+                .setContentText("You have something new in calendar")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notiManager.notify(1, notification);
+          }
+
 }
