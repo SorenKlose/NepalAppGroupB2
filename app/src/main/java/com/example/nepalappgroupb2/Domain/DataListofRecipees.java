5fc8df27a3d926204e8075e6243030549a201f15
isling98 <markushaag@live.dk>
2020-01-17 12:36:50 +0100

    lavet rigtig opskrift pdt og sat ind

--- app/src/main/java/com/example/nepalappgroupb2/Domain/DataListofRecipees.java
+++ app/src/main/java/com/example/nepalappgroupb2/Domain/DataListofRecipees.java
@@ -1,12 +1,8 @@
 package com.example.nepalappgroupb2.Domain;
 
-import android.util.ArrayMap;
-
 import com.example.nepalappgroupb2.Recipe.RecipeCardElement;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class DataListofRecipees {
     private ArrayList<RecipeCardElement> data;
@@ -27,23 +23,23 @@ public class DataListofRecipees {
 
         String[][][] test = {
                 new String[][]{
-                        new String[]{"Dal Bhat", "test_pdf.pdf"},
+                        new String[]{"Dal Bhat", "recipenepal_pdf.pdf"},
                         new String[]{"banan"}
                 },
                 new String[][]{
-                        new String[]{"Food", "test_pdf.pdf"},
+                        new String[]{"Food", "recipenepal_pdf.pdf"},
                         new String[]{ "Ingredienses" }
                 },
                 new String[][]{
-                        new String[]{"madopskrift", "test_pdf.pdf"},
+                        new String[]{"madopskrift", "recipenepal_pdf.pdf"},
                         new String[]{ "salt"}
                 },
                 new String[][]{
-                        new String[]{"Peda", "test_pdf.pdf"},
+                        new String[]{"Peda", "recipenepal_pdf.pdf"},
                         new String[]{ "nødder"}
                 },
                 new String[][]{
-                        new String[]{"god mad", "test_pdf.pdf"},
+                        new String[]{"god mad", "recipenepal_pdf.pdf"},
                         new String[]{"sovs"}
                 }
         };
