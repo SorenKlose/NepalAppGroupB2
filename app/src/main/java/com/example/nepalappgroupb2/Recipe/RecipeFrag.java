6800324865d74efcf34197fbdd5127985ae08f3f
NBK <msungo85@gmail.com>
2020-01-23 13:01:51 +0100

    searchfilterbar og recipe titel sat fast

--- app/src/main/java/com/example/nepalappgroupb2/Recipe/RecipeFrag.java
+++ app/src/main/java/com/example/nepalappgroupb2/Recipe/RecipeFrag.java
@@ -24,7 +24,7 @@ import com.example.nepalappgroupb2.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecipeFrag extends Fragment implements Observer<String>, searchWordProvider, View.OnFocusChangeListener {
+public class RecipeFrag extends Fragment implements Observer<String>, searchWordProvider {
     RecipeCardElement recipeCardElement = new RecipeCardElement();
     private final MutableLiveData<String> searchWord = new MutableLiveData<>();
     private RecyclerView recyclerView;
@@ -41,7 +41,6 @@ public class RecipeFrag extends Fragment implements Observer<String>, searchWord
         getSearchWord().observe( getActivity(), this);
 
         searcField = layout.findViewById(R.id.searchWordEditText);
-        searcField.setOnFocusChangeListener(this);
         titleView = layout.findViewById(R.id.textView6);
         recyclerView = layout.findViewById(R.id.quiz_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -147,12 +146,6 @@ public class RecipeFrag extends Fragment implements Observer<String>, searchWord
     @Override
     public void onChanged(String s) {
         adapter.getFilter().filter(s);
-    }
-
-    @Override
-    public void onFocusChange(View v, boolean hasFocus) {
-        titleView.setVisibility(View.GONE);
-        titleView.setHeight(0);
     }
 
 }
